0b939744cd79b92186db0e0ee98444687e701d52
Quvonchbek048 <quvonchbeku048@gmail.com>
2023-04-01 06:12:08 +0500

    Team Quvonchbek

--- script.js
+++ script.js
@@ -3,17 +3,14 @@ let form = document.querySelector(".form")
 fetch("http://localhost:3000/data")
     .then((res) => res.json())
     .then((data) => {
-        data.forEach(element => {
+        data.forEach((element, n, arr) => {
             let listPoragraf = document.createElement("div")
             listPoragraf.classList.add("list-poragraf")
             listPoragraf.innerHTML = `
                 <p class="list-item">${element.text}</p>
-                <button type="button" data-id="1" class="btn btn-danger remove">delete</button>
+                <button type="button" onclick="delData(${arr[n].id})" class="btn btn-danger remove">delete</button>
             `
             form.append(listPoragraf)
-        });
-        data.forEach(element => {
-            console.log(element.text);
         });
     })
 
@@ -34,10 +31,21 @@ const getData = async () => {
 }
 document.querySelector(".btn-success").onclick = () => {
     if (inp.value == "") {
-        console.log(false);
+        return
     } else {
         getData()
     }
+}
+
+function delData(id) {
+    let inp = document.querySelector(".form-control");
+    fetch(`http://localhost:3000/data/${id}`, {
+        method: "DELETE"
+    })
+        .then(res => {
+            alert("Deleted")
+        })
+    inp.value = "";
 }
 
 setInterval(() => {
